bf059e54ede6d9ce27166d15a7652499bb3447d0
Mohan Kumar BN <89718858+MBN02@users.noreply.github.com>
2022-03-15 07:30:33 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: 'nexus-user', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh 'docker build -t 40.83.250.80:8083/java-maven-app:1.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin 40.83.250.80:8083"
         sh 'docker push 40.83.250.80:8083/java-maven-app:1.0'
